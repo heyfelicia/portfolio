cb6ace9f61a44c3ef1a03d242d806d0d91117f4f
Felicia Phillips <phillipsfelicia96@gmail.com>
2026-07-25 21:40:43 -0500

    Update

--- scripts.js
+++ scripts.js
@@ -1,23 +1,6 @@
 const cursor = document.querySelector(".cursor");
 
-document.addEventListener("mousemove", (e) => {
-
-    // Move the main cursor
+window.addEventListener("mousemove", (e) => {
     cursor.style.left = e.clientX + "px";
     cursor.style.top = e.clientY + "px";
-
-    // Create a trail dot
-    const trail = document.createElement("div");
-    trail.className = "trail";
-
-    trail.style.left = e.clientX + "px";
-    trail.style.top = e.clientY + "px";
-
-    document.body.appendChild(trail);
-
-    // Remove after animation
-    setTimeout(() => {
-        trail.remove();
-    }, 200);
-
 });
